bef2c2d5ce8b89c3329645c3b8f94479c5610a66
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-08-12 08:14:44 +1000

    Initial Files + Account.java Functions

---
+++ src/Account.java
@@ -0,0 +1,62 @@
+public class Account {
+
+    private String accountNumber;
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public Account(String accountNumber, String firstName, String lastName, double balance) {
+        this.accountNumber = accountNumber;
+        this.firstName     = firstName;
+        this.lastName      = lastName;
+        this.balance       = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            amount += balance;
+        } else {
+            System.out.println("Invalid Amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            amount -= balance;
+        } else {
+            System.out.println("Invalid Amount.");
+        }
+    }
+
+    public void setBalance(double amount) {
+        if (amount > 0) {
+            amount = balance;
+        } else {
+            System.out.println("Invalid Amount.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Account Number: " + accountNumber +
+             "\nAccount Holder: " + firstName + " " + lastName +
+             "\nBalance: " + balance;
+    }
+
+}
